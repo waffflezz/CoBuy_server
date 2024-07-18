c540cedd2d172e6047123abfa7e8d2125127be67
dima <dima.skorobogatov.99@mail.ru>
2024-07-18 13:38:53 +0700

    Fix channels

--- src/routes/channels.php
+++ src/routes/channels.php
@@ -1,16 +1,18 @@
 <?php
 
-use App\Models\User;
+use App\Models\Group;
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Log;
 
-Broadcast::channel('group-changed.{group_id}', function ($user, int $groupId) {
-    return $user->groups->contains('id', $groupId);
+Broadcast::channel('group-changed.{groupId}', function ($user, string $groupId) {
+    Log::debug('GROUP-CHANGED: ' . $user->groups->contains(Group::find($groupId)));
+    return $user->groups->contains(Group::find($groupId));
 });
 
-Broadcast::channel('list-changed.{group_id}', function ($user, int $group_id) {
-    return $user->groups->contains('id', $group_id);
+Broadcast::channel('list-changed.{groupId}', function ($user, string $groupId) {
+    return $user->groups->contains(Group::find($groupId));
 });
 
-Broadcast::channel('product-changed.{group_id}', function ($user, int $group_id) {
-    return $user->groups->contains('id', $group_id);
+Broadcast::channel('product-changed.{groupId}', function ($user, string $groupId) {
+    return $user->groups->contains(Group::find($groupId));
 });
