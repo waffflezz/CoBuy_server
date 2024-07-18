465aa0d938e89da1302f6f73b3e7235da61f9659
dima <dima.skorobogatov.99@mail.ru>
2024-07-18 13:12:15 +0700

    Remove debug logs

--- src/app/Policies/ProductPolicy.php
+++ src/app/Policies/ProductPolicy.php
@@ -10,13 +10,11 @@ class ProductPolicy
 {
     public function groupMember(User $user, Group $group): bool
     {
-        Log::debug('(PRODUCT) GROUP MEMBER: user_id: ' . $user->id . ' group_id: ' . $group->id . '| user in group members = ' . $group->users->contains($user));
         return $group->users->contains($user);
     }
 
     public function groupOwner(User $user, Group $group): bool
     {
-        Log::debug('(PRODUCT) GROUP OWNER: user_id: ' . $user->id . ' group_id: ' . $group->id . '| user in group members = ' . $group->users->contains($user));
         return $user->id === $group->owner_id;
     }
 }
--- src/app/Policies/ShoppingListPolicy.php
+++ src/app/Policies/ShoppingListPolicy.php
@@ -10,13 +10,11 @@ class ShoppingListPolicy
 {
     public function groupMember(User $user, Group $group): bool
     {
-        Log::debug('(LIST) GROUP MEMBER: user_id: ' . $user->id . ' group_id: ' . $group->id . '| user in group members = ' . $group->users->contains($user));
         return $group->users->contains($user);
     }
 
     public function groupOwner(User $user, Group $group): bool
     {
-        Log::debug('(LIST) GROUP OWNER: user_id: ' . $user->id . ' group_id: ' . $group->id . '| user in group members = ' . $group->users->contains($user));
         return $user->id === $group->owner_id;
     }
 
