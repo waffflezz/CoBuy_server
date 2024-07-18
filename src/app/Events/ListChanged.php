bc0068e8216df53651051a8956d3df3780afc67f
dima <dima.skorobogatov.99@mail.ru>
2024-07-18 12:51:05 +0700

    Change broadcast method

--- src/app/Events/ListChanged.php
+++ src/app/Events/ListChanged.php
@@ -4,16 +4,13 @@ namespace App\Events;
 
 use App\Http\Resources\ShoppingListResource;
 use App\Models\ShoppingList;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class ListChanged implements ShouldBroadcastNow
+class ListChanged implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
